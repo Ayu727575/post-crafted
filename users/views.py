a0e7ad970b9e12da916e5574b52dc2d4dccf5925
Ayush Chouksey <ayush12@email.com>
2024-08-15 21:32:28 +0530

    Lecture 11 - Python Django User Authorization tutorial

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def add_user(request):
@@ -18,7 +18,15 @@ def user_login(request):
         form = AuthenticationForm(data = request.POST)
         if form.is_valid():
             login(request, form.get_user())
-            return redirect("posts:list")
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect("posts:list")
     else:
         form = AuthenticationForm()
     return render(request,"users/login.html",{"form":form})
+
+def user_logout(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("posts:list")
